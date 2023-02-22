2127bbe4cf3f92511aaa381cb0679bbf352235b3
jehmatthews <jehmatthews30@gmail.com>
2023-02-21 18:22:09 -0800

    created cypress tests for the home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,15 @@
+describe('Home', () => {
+  
+  it("should visit the home page", () => {
+    cy.visit('/');
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+  
+})
